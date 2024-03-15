1cd8c217d4e09801b5d82c3e235ce7e6f7336d2b
Arturnix <artur.zgodka.programista@gmail.com>
2024-03-15 16:05:34 +0100

    Added ViewFactory class to display main window

--- src/main/java/pl/arturzgodka/weatherapp/Launcher.java
+++ src/main/java/pl/arturzgodka/weatherapp/Launcher.java
@@ -1,20 +1,17 @@
 package pl.arturzgodka.weatherapp;
 
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.arturzgodka.weatherapp.view.ViewFactory;
 
 import java.io.IOException;
 
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("view/MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Sprawdz pogode");
-        stage.setScene(scene);
-        stage.show();
+        ViewFactory viewFactory = new ViewFactory();
+        viewFactory.showMainWindow(stage);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/arturzgodka/weatherapp/view/ViewFactory.java
@@ -0,0 +1,20 @@
+package pl.arturzgodka.weatherapp.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import pl.arturzgodka.weatherapp.Launcher;
+
+import java.io.IOException;
+
+public class ViewFactory {
+
+    public void showMainWindow(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("view/MainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Sprawdz pogode");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
